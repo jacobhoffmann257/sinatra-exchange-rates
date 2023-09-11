20f7359db38d01a59a9d0cac2caf6097dba7c21f
Jacob Hoffmann <67609232+jacobhoffmann257@users.noreply.github.com>
2023-09-11 19:03:16 +0000

    second href works

--- app.rb
+++ app.rb
@@ -17,3 +17,22 @@ get("/") do
   end
   erb(:firstcurrency)
 end
+get("/:code") do 
+  @code1 = params.fetch("code")
+  symbolsurl = "https://api.exchangerate.host/symbols" 
+  rawsymbols = HTTP.get(symbolsurl)
+  parsed_symbols = JSON.parse(rawsymbols)
+  symbols_list = parsed_symbols.fetch("symbols");
+  @symbols_array = []
+  symbols_list.each do |x,y|
+  @symbols_array.push(y)
+  end
+  @code_list = []
+  @symbols_array.each do |x,y|
+  @code_list.push( x.fetch("code"))
+  end
+erb(:secondcurrency)
+end
+get("/:code1/:code2") do
+  erb(:conversion)
+end
